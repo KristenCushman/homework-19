13ec00d3ea64908ba831e2f7f477cf5df8167c89
KristenCushman <105694983+KristenCushman@users.noreply.github.com>
2022-06-25 14:55:15 -0700

    troubleshooting
    working on it

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var helloWorld = "Hello World!";
 
 // Console.log() your variable here.
 console.log(helloWorld);
+
+$(".btn1").click(window.location="https://github.com/KristenCushman/homework-19")
