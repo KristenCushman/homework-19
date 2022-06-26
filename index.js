ac114301f7d4284e9df82b35a7c77e57f3443bfa
KristenCushman <105694983+KristenCushman@users.noreply.github.com>
2022-06-25 18:26:48 -0700

    added an event listener
    kirshner's button slides down

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ var helloWorld = "Hello World!";
 // Console.log() your variable here.
 console.log(helloWorld);
 
-$(".btn1").click(window.location="https://github.com/KristenCushman/homework-19")
+$(".btn1").click(function(){
+    window.location="https://github.com/KristenCushman/homework-19"
+})
+
+
+
+$("portfolioProjects").ready(function(){
+  $("portfolioPorjects").click(function(){
+    $("portfolioProjects").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '150px',
+      width: '150px'
+    });
+  });
+});
+
+
